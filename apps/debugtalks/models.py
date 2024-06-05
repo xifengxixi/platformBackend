d05c4fb1173747636365f328e3cc5378016a280a
曾超 <zengchao@weaver.com>
2024-06-05 17:50:13 +0800

    debugtalk字段允许空值

--- apps/debugtalks/models.py
+++ apps/debugtalks/models.py
@@ -5,7 +5,7 @@ from utils.base_models import BaseModel
 class DebugTalks(BaseModel):
     id = models.AutoField(verbose_name='id主键', primary_key=True, help_text='id主键')
     name = models.CharField('debugtalk文件名称', max_length=200, default='debugtalk.py', help_text='debugtalk文件名称')
-    debugtalk = models.TextField(null=True, default='#debugtalk.py', help_text='debugtalk.py文件')
+    debugtalk = models.TextField(null=True, blank=True, default='#debugtalk.py', help_text='debugtalk.py文件')
     project = models.OneToOneField('projects.Projects', on_delete=models.CASCADE, related_name='debugtalks', help_text='所属项目')
 
     class Meta:
